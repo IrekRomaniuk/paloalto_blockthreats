8876980e5c581a0bcddbe7e77fa8bcf5e4987caf
irom77 <irek.romaniuk@gmail.com>
2018-01-01 12:39:46 -0500

    update

--- actions/dag_update.py
+++ actions/dag_update.py
@@ -4,7 +4,7 @@ from st2actions.runners.pythonrunner import Action
 
 class UpdateDAG(Action):
     def run(self, source):
-        print(source,self.config)
+        print(source,self.config.api_key)
 
         if source == '1.1.1.1':
             return (True, source)
