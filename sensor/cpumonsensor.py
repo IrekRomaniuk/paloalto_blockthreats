1fdb3e32b360ad7fb9dfa6f523a508af9c3c61cb
irom77 <aniairom0>
2018-05-23 13:36:23 -0400

    adding cpu monitor

---
+++ sensor/cpumonsensor.py
@@ -0,0 +1,53 @@
+from st2reactor.sensor.base import PollingSensor
+from influxdb import InfluxDBClient
+import ast, requests
+
+class PanCpuMonitorSensor(PollingSensor):
+    """
+    * self.sensor_service
+        - provides utilities like
+            get_logger() for writing to logs.
+            dispatch() for dispatching triggers into the system.
+    * self._config
+        - contains configuration that was specified as
+          config.yaml in the pack.
+    * self._poll_interval
+        - indicates the interval between two successive poll() calls.
+    TESTING:
+    curl -k "https://1.1.1.1/esp/restapi.esp?type=op&cmd=<show><running><resource-monitor><seurce-monitor></running></show>&key="    
+    """
+    def __init__(self, sensor_service, config, poll_interval): #sensor_service, config, poll_interval, 
+        super(PanCpuMonitorSensor, self).__init__(sensor_service=sensor_service,  
+                                          config=config,                                 
+                                          poll_interval=poll_interval)
+        self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
+
+        
+    def setup(self):
+        self._key = self._config['api_key'] # or None
+        self._url = self._config['url']
+        self._ips = self._config['ips']          
+
+
+    def poll(self):        
+        self._logger.debug('PanCpuMonitorSensor dispatching trigger...')
+        payload = {}
+        for ip in _ips:
+            response = requests.get('https://'+ip+_url_+_key, verify=False)
+            self._logger.debug('response {}'.format(response.text))
+            payload[ip]=response.status_code
+        #requests.get("https://hchk.io/")
+        self.sensor_service.dispatch(trigger="pan.cpu_monitor", payload=payload)     
+
+    def cleanup(self):
+        pass
+
+    # Methods required for programmable sensors.
+    def add_trigger(self, trigger):
+        pass
+
+    def update_trigger(self, trigger):
+        pass
+
+    def remove_trigger(self, trigger):
+        pass
