a861d967dfd648a47f42ea6317a92e76b8a8aebd
irom77 <aniairom0>
2018-05-24 16:49:05 -0400

    fixing

--- actions/influx_write.py
+++ actions/influx_write.py
@@ -29,7 +29,8 @@ class influx_write(Action):
         _base_url, _port = self.config['base_url'].split(":")
         client = InfluxDBClient(_base_url, _port, _user, _pass, _db)
         # print(_db,_user,_pass,_base_url, _port)
-        result=client.write_points(payload)
+        print(payload['10.34.2.20'])
+        result=client.write_points(payload['10.34.2.20'])
         client.close()
         return result
 
