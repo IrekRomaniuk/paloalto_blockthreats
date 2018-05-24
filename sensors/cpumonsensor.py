0e649c3ec7bee38fa5c0ef94edd62679bf83c10c
irom77 <aniairom0>
2018-05-24 15:13:59 -0400

    fixing

--- sensors/cpumonsensor.py
+++ sensors/cpumonsensor.py
@@ -61,7 +61,7 @@ class PanCpuMonitorSensor(PollingSensor):
                         payload['fields'][self._val]=max([int(value) for value in cpu[i]['value'].split(',')])
                         self._logger.debug('#### Payload: {}'.format(payload))
                         points.append(payload)
-        self._logger.debug('#### Dispatching payload...')                                        
+        self._logger.debug('#### Dispatching payload...')                                    
         self.sensor_service.dispatch(trigger="pan.cpu_mon_trigger", payload=','.join(points))                
         #requests.get("https://hchk.io/")
              
