86e77b9b0222b4f5b4954670e36b47b814826338
irom77 <aniairom0>
2018-05-24 11:27:59 -0400

    action added

---
+++ actions/influx_write.py
@@ -0,0 +1,16 @@
+from influxdb import InfluxDBClient
+
+from st2actions.runners.pythonrunner import Action
+
+
+class influx_write(Action):
+       
+    def run(self, trigger): #, tags
+        _db = self.config['db']
+        _user = self.config['username']
+        _pass = self.config['password']
+        _base_url, _port = self.config['base_url'].split(":")
+        client = InfluxDBClient(_base_url, _port, _user, _pass, _db)
+        print(_db,_user,_pass,_base_url, _port)
+        client.close()
+    return trigger
